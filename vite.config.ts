0373180aff8c154c7fae28a88a7015bead0eef9e
Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>
2025-12-14 23:32:02 -0500

    Add GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/portfolio/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
@@ -58,3 +59,5 @@
       open: true,
     },
   });
+
+  
